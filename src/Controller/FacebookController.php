e9260f26a1fd92d47146c677d044732cd7375e7f
Nicole Wolf <nicole.wolf@8mylez.com>
2023-05-04 19:31:20 +0200

    add facebook login authenticator

---
+++ src/Controller/FacebookController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FacebookController extends AbstractController
+{
+    /**
+     *
+     *
+     * @Route("/connect/facebook", name="connect_facebook_start")
+     */
+    public function connectAction(ClientRegistry $clientRegistry)
+    {
+        return $clientRegistry
+            ->getClient('facebook_main')
+            ->redirect([
+                'public_profile', 'email'
+            ]);
+    }
+
+    /**
+     * @Route("/connect/facebook/check", name="connect_facebook_check")
+     */
+    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
+    {
+        
+    }
+}
